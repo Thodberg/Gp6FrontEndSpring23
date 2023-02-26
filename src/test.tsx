a9f6d8b9f074693a5e2b30aab0bf926ac98a727c
Emmanuel Baime <s215790@dtu.dk>
2023-02-26 20:23:31 +0100

    Create test.tsx

---
+++ src/test.tsx
@@ -0,0 +1,8 @@
+function ShoppingList(){
+
+const products = [
+  { name: 'Rose', price: "40 kr", id: 1 },
+  { name: 'Scheffler', price: "35 kr", id: 2 },
+  { name: 'Pions', price: "230", id: 3 },
+];
+}
